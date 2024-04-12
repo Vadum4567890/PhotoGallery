54851c3f584d5258570f0ba8ece6f4be0d8ca614
vadimyff <solpo789@gmail.com>
2024-04-12 11:25:26 +0300

    changing

--- client-gallery/src/components/Footer/Footer.jsx
+++ client-gallery/src/components/Footer/Footer.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import styles from '../../styles/Footer.module.css';
 
-import TELEGRAM from '../../images/footer/itelegram.svg';
-import FACEBOOK from '../../images/footer/ifacebook.svg';
-import INSTAGRAM from '../../images/footer/iinstagram.svg';
-import LOGO from '../../images/logo.svg';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../../utils/routes';
 
